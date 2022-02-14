c244edc1bdb637e79fbabbaafef41fbb3ddfd942
Chisom <chisomije92@gmail.com>
2022-02-14 23:21:23 +0100

    implement mongoose models

--- models/orders.js
+++ models/orders.js
@@ -1,32 +1,28 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema(
+const OrderSchema = new mongoose.Schema(
   {
-    title: {
+    customer: {
       type: String,
       required: true,
       maxlength: 60,
     },
-    desc: {
+    address: {
       type: String,
       required: true,
       maxlength: 200,
     },
-    img: {
-      type: String,
+    total: {
+      type: Number,
       required: true,
     },
-    prices: {
-      type: [Number],
-      required: true,
+    status: {
+      type: Number,
+      default: 0,
     },
-    extrasOptions: {
-      type: [
-        {
-          text: { type: String, required: true },
-          price: { type: Number, required: true },
-        },
-      ],
+    method: {
+      type: Number,
+      required: true,
     },
   },
   { timestamps: true }
