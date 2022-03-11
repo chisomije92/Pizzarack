2ed131375c8b73fb9aeba0523ffbf43fd7ad0afc
Chisom <chisomije92@gmail.com>
2022-03-11 22:12:20 +0100

    modify

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -9,8 +9,6 @@ const handler = async (req, res) => {
     try {
       const products = await Product.find();
       res.status(200).json(products);
-
-      // res.end();
     } catch (err) {
       res.status(500).json(err);
     }
