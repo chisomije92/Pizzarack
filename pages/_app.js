8f3c0792b0433dfe8b47fec428c7a44f342b59cd
Chisom Ijeomah <chisomije92@gmail.com>
2022-09-02 20:09:26 +0100

    revise changes - favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
@@ -6,19 +8,19 @@ import { SessionProvider } from "next-auth/react";
 import store from "../store/cartSlice";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Layout>
-          <Head>
-            <title>Pizza Rack Pizzas</title>
-            <link rel="icon" href="/pizza_snack_icon.svg" />
-          </Head>
-          <Component {...pageProps} />
-        </Layout>{" "}
-      </Provider>
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<Provider store={store}>
+				<Layout>
+					<Head>
+						<title>Pizza Rack Pizzas</title>
+						<link rel="icon" href="/favicon.ico" />
+					</Head>
+					<Component {...pageProps} />
+				</Layout>{" "}
+			</Provider>
+		</SessionProvider>
+	);
 }
 
 export default MyApp;
